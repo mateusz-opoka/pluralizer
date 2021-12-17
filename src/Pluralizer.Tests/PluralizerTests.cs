f723b5ff413f6654a15f432b528be9d1cd78294b
Mateusz Opoka <mateusz@mobiloc.pl>
2021-12-17 17:59:23 +0100

    added format method

---
+++ src/Pluralizer.Tests/PluralizerTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace Pluralizer.Tests
+{
+    [TestFixture]
+    public class PluralizerTests
+    {
+        [TestCase(0, ExpectedResult = "dni")]
+        [TestCase(1, ExpectedResult = "dzień")]
+        [TestCase(2, ExpectedResult = "dni")]
+        [TestCase(4, ExpectedResult = "dni")]
+        [TestCase(5, ExpectedResult = "dni")]
+        [TestCase(22, ExpectedResult = "dni")]
+        [TestCase(100, ExpectedResult = "dni")]
+        [TestCase(212, ExpectedResult = "dni")]
+        public string Format_OnPolishNoun_WithoutGenetiveForm_ShouldReturnCorrectForm(int n) =>
+            new Pluralizer("dzień", "dni").Format(n);
+
+        [TestCase(0, ExpectedResult = "testów")]
+        [TestCase(1, ExpectedResult = "test")]
+        [TestCase(2, ExpectedResult = "testy")]
+        [TestCase(4, ExpectedResult = "testy")]
+        [TestCase(5, ExpectedResult = "testów")]
+        [TestCase(10, ExpectedResult = "testów")]
+        [TestCase(100, ExpectedResult = "testów")]
+        [TestCase(101, ExpectedResult = "testów")]
+        [TestCase(102, ExpectedResult = "testy")]
+        [TestCase(212, ExpectedResult = "testów")]
+        [TestCase(315, ExpectedResult = "testów")]
+        public string Format_OnPolishNoun_WithGenetiveForm_ShouldReturnCorrectForm(int n) =>
+            new Pluralizer("test", "testy", "testów").Format(n);
+    }
+}
--- src/Pluralizer/Pluralizer.cs
+++ src/Pluralizer/Pluralizer.cs
@@ -2,9 +2,22 @@
 {
     public class Pluralizer
     {
-        public string Format(int value)
+        private readonly string[] _forms;
+
+        public Pluralizer(string singularNominativ, string pluralNominativ)
+            : this(singularNominativ, pluralNominativ, pluralNominativ)
+        { }
+
+        public Pluralizer(string singularNominativ, string pluralNominativ, string pluralGenitive)
         {
-            return value.ToString();
+            _forms = new[] { singularNominativ, pluralNominativ, pluralGenitive };
+        }
+
+        public string Format(int n)
+        {
+            if (n == 1) return _forms[0];
+            if (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)) return _forms[1];
+            return _forms[2];
         }
     }
 }
